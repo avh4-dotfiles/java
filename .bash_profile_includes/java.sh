b0398b74abad2d356e8e4361dc8e6064ccd2d921
Aaron VonderHaar <gruen0aermel@gmail.com>
2013-12-07 13:06:13 -0500

    Add JAVA6_HOME, JAVA7_HOME; play-2.1.1 to PATH

---
+++ .bash_profile_includes/java.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export JAVA7_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_45.jdk/Contents/Home/
+export JAVA6_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home/
---
+++ .bash_profile_includes/play.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export PATH="$PATH:$HOME/bin/play-2.1.1"
